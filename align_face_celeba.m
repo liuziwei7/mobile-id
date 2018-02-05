b6307f017b1b4d9e78c877303fe8a32a682cf5a8
Ziwei Liu <zwliu.hust@gmail.com>
2018-02-04 18:15:08 -0800

    add face alignment code

---
+++ align_face_celeba.m
@@ -0,0 +1,25 @@
+function [img_align, landmarks_align] = align_face_celeba(img_orig, landmarks_orig)
+%% This MATLAB function provides the face alignment method used in generating CelebA dataset ('align&cropped images').
+%
+%% Args:
+% 'img_orig': the original face image
+% 'landmarks_orig': the position of five facial landmarks on the original face image, which should be given in the following form:
+%
+%  landmarks_orig = [lefteye_x     lefteye_y
+%                    righteye_x    righteye_y
+%                    nose_x        nose_y
+%                    leftmouth_x   leftmouth_y
+%                    rightmouth_x  rightmouth_y];
+%
+%% Returns:
+% 'img_align': the aligned face image with the size of 218*178
+% 'landmarks_align': the position of five facial landmarks on the aligned face image
+
+	pos_align = [70.745     112
+		  		 108.237    112
+	      		 89.432     153.514];
+
+	t = cp2tform([landmarks_orig([1 2], :); mean(landmarks_orig([4 5], :))], pos_align, 'nonreflective similarity');
+	r = makeresampler({'cubic', 'nearest'}, 'replicate');
+	img_align = imtransform(img_orig, t, r, 'XYScale', 1, 'XData', [1 178], 'YData', [1 218]);
+	landmarks_align = tformfwd(t, landmarks_orig);
